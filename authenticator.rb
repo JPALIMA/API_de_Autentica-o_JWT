16fe426bb0be9539270675929f4935dc17940149
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-09-09 16:35:45 -0300

    criacao 'authenticator.rb'para  gerar tokens JWT para autenticação de usuários em uma API Ruby

--- authenticator.rb
+++ authenticator.rb
@@ -1,6 +1,6 @@
 require 'jwt'
 
-#Simples banco de dados de usuários (substitua por banco de dados real )
+#Simples banco de dados de usários (substitua por banco de dados real )
 USERS = [
   { username: 'usuario1', password: 'senha1'},
   { username: 'usuario2', password: 'senha2'}
@@ -11,10 +11,10 @@ SECRET_KEY = 'sua_chave_secreta'
 
 #Método para autenticar um usuário e gerar um token JWT
 def authenticate(username, password)
-  user = USERS.find { |u| u[:username] == username && u[:password] == password }
-  return nil unless user
+  user = USERS.find { |u| u[:username] == username && u[:password] == password}
+  return nill unless user
 
   payload = { username: username}
   token = JWT.encore(payload, SECRET_KEY, 'HS256')
-  token 
+  token
 end
